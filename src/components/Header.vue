cd8eb045257e4f11376b3199e7ad8abfde7ce578
kevin munoz <harquin09@gmail.com>
2023-06-08 07:31:55 -0500

    empezando

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref, reactive } from "vue";
+import { db } from "../data/guitarras";
+const state = reactive({
+  guitarras: db,
+});
+console.log("guitarras", state.guitarras);
+console.log(state.guitarras);
+</script>
+<template>
+  <div></div>
+</template>
+
+<style lang="scss" scoped></style>
